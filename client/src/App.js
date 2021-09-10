2d35ede773937ebea3a0aa5344ed47a3afaaf193
Logan <loganvk18@gmail.com>
2021-09-10 11:48:21 +0530

    completed homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
-
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import HomePage from './Screens/Home/HomePage';
 
 function App() {
   return (
-    <div>Hello logan</div>
+    <Router>
+      <Route path="/" exact><HomePage /></Route>
+    </Router>
   );
 }
 

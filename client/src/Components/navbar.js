cd0eaf6061bbd0750ece1d0e80b64bd6602f15f9
Naveenkumar M <naveen9715568487@gmail.com>
2021-09-12 13:30:10 +0530

    added all

--- client/src/Components/navbar.js
+++ client/src/Components/navbar.js
@@ -2,11 +2,15 @@ import React from 'react'
 import profile from '../Assets/profile.png'
 import '../Styles/navbar.css'
 import logo from '../Assets/site-logo.png'
+import {Link,useHistory} from 'react-router-dom'
 function Navbar() {
     return (
         <div className="navbar">
             <div className="site-logo">
                <img src={logo} alt="v" width="100px" height="auto"/>
+            </div>
+            <div className="routes">
+                <Link to="/Searchworkers" className="link">Searchworkers</Link>
             </div>
             <div className="menu">
                 <img src={profile} alt="profile"  width="35px"/>
